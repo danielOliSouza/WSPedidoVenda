70480300ff84fe098afa0d8de854072d211d302a
Daniel <Daniel@Daniel-PC>
2017-05-22 16:30:12 -0300

    Correção classe Cliente.java

--- pedidovenda/src/main/java/br/com/adsddm/pedidovenda/model/Cliente.java
+++ pedidovenda/src/main/java/br/com/adsddm/pedidovenda/model/Cliente.java
@@ -3,11 +3,6 @@ package br.com.adsddm.pedidovenda.model;
 import java.io.Serializable;
 import javax.persistence.*;
 
-
-/**
- * The persistent class for the cliente database table.
- * 
- */
 @Entity
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
